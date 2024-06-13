8b839179660c68a611aa05f106527a60afe9e23b
SindhuKota7318 <s_kota192804@fanshaweonline.ca>
2024-06-12 20:13:59 -0400

    lab assessment1

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+console.log()
+{
+    const myFullName = SindhuKota;
+    const myStudentNumber = 1153251;
+}
+const result = `SindhuKota-${1153251}`;
+console.log(result);
+const headerContent = document.querySelector('#h1');
+
