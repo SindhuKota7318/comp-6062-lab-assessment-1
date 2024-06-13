9f3923482b59b37c9ea02c2b92197b7a007b0237
SindhuKota7318 <s_kota192804@fanshaweonline.ca>
2024-06-12 21:01:12 -0400

    labassessment1

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
-console.log()
-{
-    const myFullName = SindhuKota;
-    const myStudentNumber = 1153251;
-}
-const result = `SindhuKota-${1153251}`;
+console.log('')
+const myFullName = 'SindhuKota';
+const myStudentNumber = '1153251';
+const result = `${myFullName}-${myStudentNumber}`;
 console.log(result);
-const headerContent = document.querySelector('#h1');
-
+let headerContent = document.querySelector('h1');
+headerContent.innerHTML = result;
+headerContent.classList.add('headingPrimary');
